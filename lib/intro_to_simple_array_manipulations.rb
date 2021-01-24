656b16d734980ac69ef676897d1ea75e1764f767
Jabair Khan <jabairk@gmail.com>
2021-01-24 18:02:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,9 +25,7 @@ def shift_with_args (ice_cream_brands)
 end
 
 def using_concat (my_favorite_things, more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  all_my_favs = my_favorite_things.concat (more_favs)
+ my_favorite_things.concat(more_favs)
 end
 
 def using_insert (list_of_programming_languages, another_language)
