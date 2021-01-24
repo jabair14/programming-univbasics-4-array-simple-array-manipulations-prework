fe5a7ce91bea63f5ce0f03cfd4dbd0c9caa2be43
Jabair Khan <jabairk@gmail.com>
2021-01-24 17:48:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,7 @@ end
 def pop_with_args (dog_breeds)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   small_dogs = ["Chihuahua", "Shiba Inu"] 
-  dog_breeds.pop(small_dogs) 
+  dog_breeds.pop(2,small_dogs) 
 end
 
 def using_shift (my_favorite_cities)
