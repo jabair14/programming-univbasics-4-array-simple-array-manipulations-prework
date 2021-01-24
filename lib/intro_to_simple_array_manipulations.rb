6b06b2370ad248d0d1f41930a972cca94ae210c0
Jabair Khan <jabairk@gmail.com>
2021-01-24 17:31:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,7 +36,7 @@ def using_insert (list_of_programming_languages, another_language)
 end
 
 def using_uniq (haircuts)
-  haircuts.uniq ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  haircuts.uniq! 
 end
 
 
